9a886e4e9a864cf1331022e0252b2e8b1bbda401
LaineyC <272874065@qq.com>
2016-12-17 21:20:55 +0800

    优化代码

--- blackdream/src/main/java/com/lite/blackdream/business/parameter/generatorinstance/GeneratorInstanceRunResponse.java
+++ blackdream/src/main/java/com/lite/blackdream/business/parameter/generatorinstance/GeneratorInstanceRunResponse.java
@@ -1,17 +1,18 @@
 package com.lite.blackdream.business.parameter.generatorinstance;
 
+import com.lite.blackdream.business.domain.RunResult;
 import com.lite.blackdream.framework.model.Response;
 
 /**
  * @author LaineyC
  */
-public class GeneratorInstanceRunResponse extends Response<Object> {
+public class GeneratorInstanceRunResponse extends Response<RunResult> {
 
     public GeneratorInstanceRunResponse(){
 
     }
 
-    public GeneratorInstanceRunResponse(Object body){
+    public GeneratorInstanceRunResponse(RunResult body){
         setBody(body);
     }
 
